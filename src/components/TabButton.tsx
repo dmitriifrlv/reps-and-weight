af9630625565014d1cfe3d771e7f80c43b1e5236
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-01-01 19:37:46 +0500

    added tab button component

---
+++ src/components/TabButton.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+type TabButtonProps = {
+  text: string;
+  icon: JSX.Element;
+  onClick: () => void;
+};
+const NeoTabButton = styled.button<{ darkMode: boolean }>`
+  cursor: pointer;
+  border: none;
+  height: 100%;
+  width: 50%;
+  background: transparent;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  font-weight: 600;
+  gap: 0.5rem;
+  box-shadow: ${({ darkMode, theme }) =>
+    darkMode ? theme.shadows.dark.bsDark : theme.shadows.light.bsLight};
+  &:hover {
+    box-shadow: ${({ darkMode, theme }) =>
+      darkMode
+        ? theme.shadows.dark.bsDarkHoverTab
+        : theme.shadows.light.bsLightHover};
+  }
+  &:active {
+    box-shadow: ${({ darkMode, theme }) =>
+      darkMode
+        ? theme.shadows.dark.bsDarkActive
+        : theme.shadows.light.bsLightActive};
+  }
+  clip-path: inset(-10px 0px 0px 0px);
+  @media (max-width: 600px) {
+    flex-direction: column;
+    font-size: 1rem;
+    gap: 4px;
+  }
+`;
+
+export const TabButton = ({ icon, text, onClick }: TabButtonProps) => {
+  return (
+    <NeoTabButton darkMode={true} onClick={onClick}>
+      {icon}
+      {text}
+    </NeoTabButton>
+  );
+};
