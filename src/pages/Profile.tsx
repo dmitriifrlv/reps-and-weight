d2738c488c7b19e88631dee6f5517503e84461c1
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-02-08 19:50:32 +0500

    changed styles of profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,9 @@
-import { Layout } from "../components/Layout";
 import { useGetUserInfoQuery } from "../app/service";
 import styled from "@emotion/styled";
-import { NeoButton } from "../components";
-import { MdExitToApp } from "react-icons/md";
-import { useContext } from "react";
-import { AuthContext } from "../app/AuthContext";
+import { Layout, Header, Main, Navigation } from "../components/Layout.styles";
+import { useNavigate } from "react-router-dom";
+import { ActionIcon } from "@mantine/core";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const ProfileSection = styled.main`
   display: flex;
@@ -18,31 +17,39 @@ const ProfileInfo = styled.span`
 
 export const Profile = () => {
   const { isSuccess, data, error, isLoading } = useGetUserInfoQuery();
-  const { logout } = useContext(AuthContext);
-  console.log(data);
-  console.log(new Date(data.createdAt).toLocaleString("ru-RU").slice(0, 10));
+  const navigate = useNavigate();
+
   return (
     <Layout>
-      {error ? (
-        <p>There was an error</p>
-      ) : isLoading ? (
-        <p>Warming up ...</p>
-      ) : isSuccess ? (
-        <ProfileSection>
-          <p>
-            Email: <ProfileInfo>{data.email}</ProfileInfo>
-          </p>
-          <p>
-            User since:{" "}
-            <ProfileInfo>
-              {new Date(data.createdAt).toLocaleString("ru-RU").slice(0, 10)}
-            </ProfileInfo>
-          </p>
-          <p>
-            Total workouts: <ProfileInfo>{data.workouts.length}</ProfileInfo>
-          </p>
-        </ProfileSection>
-      ) : null}
+      <Header>
+        <Navigation>
+          <ActionIcon onClick={() => navigate(-1)} size="lg">
+            <AiOutlineArrowLeft size="24px" />
+          </ActionIcon>
+        </Navigation>
+      </Header>
+      <Main>
+        {error ? (
+          <p>There was an error</p>
+        ) : isLoading ? (
+          <p>Warming up ...</p>
+        ) : isSuccess ? (
+          <ProfileSection>
+            <p>
+              Email: <ProfileInfo>{data.email}</ProfileInfo>
+            </p>
+            <p>
+              User since:{" "}
+              <ProfileInfo>
+                {new Date(data.createdAt).toLocaleString("ru-RU").slice(0, 10)}
+              </ProfileInfo>
+            </p>
+            <p>
+              Total workouts: <ProfileInfo>{data.workouts.length}</ProfileInfo>
+            </p>
+          </ProfileSection>
+        ) : null}
+      </Main>
     </Layout>
   );
 };
