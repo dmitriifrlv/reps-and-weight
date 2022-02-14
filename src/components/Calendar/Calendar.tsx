0261d22fbdef7a18061ea78b445e9f434eb6e553
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-02-14 20:14:52 +0500

    changed calendar styles, added modal

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,13 +1,20 @@
 import styled from "@emotion/styled";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../Styles/ThemeContext";
 import { DayPicker } from "react-day-picker";
 import { CardProps } from "../../Types/StyledElementsTypes";
 import enGb from "date-fns/locale/en-GB";
 import { WorkoutDataType } from "../../Types/WorkoutTypes";
-import { FaDumbbell } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { FaDumbbell, FaPlus } from "react-icons/fa";
+import { useNavigate, Link } from "react-router-dom";
 import calendarClassNames from "./Calendar.module.css";
+import { Modal } from "@mantine/core";
+
+const WorkoutCell = styled.div`
+  label: WorkoutCell;
+  width: 100%;
+  height: 100%;
+`;
 
 type CalendarProps = {
   workouts: WorkoutDataType[];
@@ -18,7 +25,6 @@ const CalendarCard = styled.div<CardProps>`
     darkMode ? theme.shadows.dark.bsDark : theme.shadows.light.bsLight};
   transition: box-shadow 0.2s ease;
   border-radius: 1rem;
-  /* height: 100%; */
   @media (max-width: 768px) {
     border-radius: 0;
   }
@@ -37,11 +43,33 @@ const CalendarContainer = styled.div`
 
 const CalendarCell = styled.div`
   cursor: pointer;
+  width: 100%;
+  height: 100%;
+  .addNewWorkout {
+    display: none;
+  }
+  &:hover {
+    background: #3d3d3d;
+    .addNewWorkout {
+      display: flex;
+    }
+  }
+`;
+
+const CalendarDate = styled.p``;
+
+const WorkoutIconContainer = styled.div`
+  height: calc(100% - 24px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Calendar = ({ workouts }: CalendarProps) => {
   const { darkMode } = useContext(ThemeContext);
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+
   const formatDay = (day: Date) => {
     const date = day.getDate();
     const month = day.getMonth();
@@ -60,28 +88,69 @@ export const Calendar = ({ workouts }: CalendarProps) => {
       localStorage.setItem("date", JSON.stringify(day));
       navigate("/workout");
     };
+    switch (foundWorkoutDay.length) {
+      case 0:
+        return (
+          <CalendarCell onClick={setNewDate}>
+            {date}
+            {
+              <WorkoutIconContainer>
+                <FaPlus className="addNewWorkout" />
+              </WorkoutIconContainer>
+            }
+          </CalendarCell>
+        );
+      case 1:
+        return (
+          <WorkoutCell
+            onClick={() => navigate(`/workout/${foundWorkoutDay[0]._id}`)}
+          >
+            <CalendarCell key={foundWorkoutDay[0]._id}>
+              <CalendarDate className="calendarDate">{date}</CalendarDate>
 
-    return (
-      <>
-        {foundWorkoutDay?.length > 0 ? (
-          <div>
-            {foundWorkoutDay?.map((i) => (
-              <CalendarCell
-                onClick={() => navigate(`/workout/${i._id}`)}
-                key={i._id}
-              >
-                <p className="calendarDate">{date}</p>
-                <div className="workoutDate">
-                  <FaDumbbell />
-                </div>
+              <WorkoutIconContainer>
+                <FaDumbbell color="#b43919" />
+              </WorkoutIconContainer>
+            </CalendarCell>
+          </WorkoutCell>
+        );
+      default:
+        return (
+          <>
+            <WorkoutCell onClick={() => setOpen(true)}>
+              <CalendarCell>
+                <CalendarDate className="calendarDate">{date}</CalendarDate>
+                <WorkoutIconContainer>
+                  <FaDumbbell color="#b43919" />
+                </WorkoutIconContainer>
               </CalendarCell>
-            ))}
-          </div>
-        ) : (
-          <CalendarCell onClick={setNewDate}>{date}</CalendarCell>
-        )}
-      </>
-    );
+            </WorkoutCell>
+            <Modal
+              classNames={{
+                title: "text-xl",
+              }}
+              opened={open}
+              onClose={() => setOpen(false)}
+              title="Choose a workout:"
+            >
+              {foundWorkoutDay.map((workout, index) => {
+                return (
+                  <Link
+                    to={`/workout/${workout._id}`}
+                    key={workout._id}
+                    className="modalLink"
+                  >
+                    Muscle Groups:{" "}
+                    {workout.muscleGroups.map((muscleGroup) => (
+                      <span>{muscleGroup}, </span>
+                    ))}
+                  </Link>
+                );
+              })}
+            </Modal>
+          </>
+        );
+    }
   };
 
   return (
